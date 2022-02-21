df1901bd09868e0f008bdff35417c7b4a63c97a1
alesskorjanc <ales.skorjanc@bf.uni-lj.si>
2022-02-21 15:13:45 +0100

    Comments updated and modified

--- gaussianSignal.m
+++ gaussianSignal.m
@@ -1,108 +1,112 @@
-function [signal,time,mag,freq,typein]=gaussianSignal(typein,magin,freqin,duration,fs)
+function [signalOut,timeOut,magOut,freqOut,typeIn]=gaussianSignal(typeIn,magIn,freqIn,duration,fs)
 
-% [signal,time]=gaussianSignal(frequency,amplitude,tmax,fs)
+% [signalOut,timeOut,magOut,freqOut,typeIn]=gaussianSignal(typeIn,magIn,freqIn,duration,fs)
+%
 % 
 % Function returns a time-domain (TD) signal with user-defined spectral
-% properties, using inverse fast Fourier transform (ifft).
+% properties in frequency-domain (FD), using inverse fast Fourier transform
+% (ifft).
+%
 % Created by Ales Skorjanc 18.2.2022
 %
 %% Inputs:
 % 
-% typein: string
-%   user defined type of the input spectrum, 'amplitude' or 'power' 
+% typeIn: string
+%   user defined type of the input frequency spectrum, 'amplitude' or
+%   'power'
 %
-% magin... a row array of user defined amplitudes of the signal frequency
-% spectrum, expersed as spectral density per unit bandwidth (1/N), if type
-% 'amplitude' is chosen; or signal power spectral density (psd) if type
-% 'power' is chosen
+% magIn: row vector
+%   user defined amplitude of the signal frequency spectrum, expressed as
+%   amplitude spectral density (asd) per unit bandwidth (1/N), if type
+%   'amplitude' is chosen; or signal power spectral density (psd) per
+%   frequency bandwidth (Hz) if type 'power' is chosen
 %
-% freqin... a row array of frequencies at which the signal amplitudes or psd
-% are defined. Vectors magin and freqin must be of the same length and
-% the values in frequency must increase monotonically.
+% freqIn: row vector
+%   frequencies at which the signal asd or psd values are defined. Vectors
+%   magIn and freqIn must be of the same length and the values in freqIn
+%   must increase monotonically.
 %
-% duration... the duration of the output TD signal in seconds
+% duration: double
+%   duration of the output TD signal in seconds
 %
-% fs... the sampling frequency of the TD signal in Hz 
+% fs: double
+%   sampling frequency of the output TD signal in Hz 
 %
 %
 %% Outputs: 
 %
-% signal... output TD signal with the standard deviation of the amplitude
-% normalized to 1.
+% signalOut: row vector
+%   output TD signal with the standard deviation of the amplitude
+%   normalized to 1
 %
-% time... array of TD signal time points
+% timeOut: row vector
+%   time points at which TD signal was calculated
 %
-% mag... array of user defined input amplitudes or psd of signal
-% frequency spectrum, interpolated to frequency values in freq
+% magOut: row vector
+%   user defined input asd or psd, interpolated to frequency values in
+%   freqOut
 %
-% freq... array of equally spaced frequencies from 0 to fs/2 Hz
+% freqOut: row vector
+%   equally spaced frequencies from 0 to fs/2 Hz
 %
-% type... type of the input frequency spectrum, 'amplitude' or 'power'
+% typeIn: string
+%   type of the input frequency spectrum, 'amplitude' or 'power'
 % 
-% Outputs mag, freq and type can be used with function drawGaussianSignal()
-% to visualize the input frequency spectrum and the results.
+% Outputs magOut, freqOut and typeIn can be used with function
+% drawGaussianSignal() to visualize the input frequency spectrum and the
+% results of the function gaussianSignal.
 
 %% generate the TD signal using ifft
 
 N = round(duration*fs); % length of the output TD signal
 fN = fs/2; % Nyquist frequency
-dt = 1/fs;  
-% dt...time resolution of the signal in TD
+dt = 1/fs; % time resolution of the signal in TD
 
-fftL = round(N/2);  
-% fftL...length of the signal at positive frequencies in FD 
-df = fN / fftL; 
-% df...frequency resolution in FD 
+fftL = round(N/2); % length of the signal in FD at positive frequencies 
+df = fN / fftL; % frequency resolution in the FD 
 
-freq = 0:df:(fN-df);   
-% freq...positive frequencies in FD from 0 to fN in Hz 
+freqOut = 0:df:(fN-df); % positive frequencies in FD from 0 to fN in Hz 
 
+% user defined signal asd or psd interpolated to frequencies in freqOut
+magOut =  interp1(freqIn,magIn,freqOut); 
 
-mag =  interp1(freqin,magin,freq); 
-% mag...user defined signal frequency spectrum amplitude or psd
-% interpolated to frequencies in freq
-mag(isnan(mag)) = 0;  
 % substitute NaN values, which interp1 returns for points lying outside the
 % frequency range defined by the user
+magOut(isnan(magOut)) = 0;  
 
-
-if strcmp(typein,'amplitude')
-    % if user defines frequency spectrum amplitude, convert to power and
-    % normalize, such that the integral of psd over frequency range equals
-    % 1, giving TD signal variance 1
-    fftP = mag.^2/(sum(mag.^2)*df); 
-elseif strcmp(typein,'power')
-    fftP = mag/(sum(mag)*df);
-    % if user defines frequency spectrum psd, normalize such that the
-    % integral of psd over frequency range equals 1, giving TD signal
-    % variance 1
+% if user defines asd, convert to power ()^2 and normalize, such that the
+% integral of psd over the frequency range 0 to fN Hz equals 1, giving TD
+% signal variance 1. If user defines psd, only normalize. 
+if strcmp(typeIn,'amplitude')
+    fftP = magOut.^2/(sum(magOut.^2)*df);   
+elseif strcmp(typeIn,'power')
+    fftP = magOut/(sum(magOut)*df);
 end;
 
+% transform psd to asd, which is the input into the ifft algorithm. Factor
+% N comes from Parseval's theorem to ensure that the integral over the
+% power spectrum equals the energy of the signal. Factor 2 is needed to
+% take into account the doubling of the number of values in line 39.
 fftA = sqrt(fftP*df/2)*N; 
-% transform psd to amplitude density, which is the input into the ifft
-% algorithm. Factor N comes from Parseval's theorem to ensure that the
-% integral over the power spectrum equals the energy of the signal. Factor 2
-% is needed to take into account the doubling of the number of values in
-% line 39.
 
-% generate random phases of FD signal:
-phase = rand(1,fftL); 
+phase = rand(1,fftL); % generate random phases of FD signal
 
-% calculate complex FD signal with user defined amplitudes or psd and
-% random phases:
+% calculate complex FD signal with user defined asd or psd and random
+% phases
 signalFD = fftA.*exp(1i*phase*2*pi()); 
 
-signalFDneg = fliplr(conj(signalFD)); 
-% generate FD signal values at negative frequencies
-signalFD = [signalFD,signalFDneg]; 
-% concatenate positive and negative frequencies
-signalFD = [0,signalFD]; 
-% add zero to the beginning of the complex signal
-signal = ifft(signalFD); 
+signalFDneg = fliplr(conj(signalFD)); % FD signal values at negative frequencies
+
+signalFD = [signalFD,signalFDneg]; % concatenate positive and negative frequencies
+
+signalFD = [0,signalFD]; % add zero to the beginning of the complex signal
+
 % calculate output TD signal from complex values of the signal in FD using
 % ifft
-time = 0:dt:dt*(length(signal)-1); 
-% generate vector array of times at which the TD signal is defined
+signalOut = ifft(signalFD); 
 
+% generate row vector of times at which the TD signal is defined
+timeOut = 0:dt:dt*(length(signalOut)-1); 
 
-plotGaussianSignal(signal,time,mag,freq,typein)
+% plot the results
+plotGaussianSignal(signalOut,timeOut,magOut,freqOut,typeIn)
--- plotGaussianSignal.m
+++ plotGaussianSignal.m
@@ -1,61 +1,67 @@
-function plotGaussianSignal(signal,time,mag,freq,typein)
+function plotGaussianSignal(signal,time,mag,freq,typeIn)
 
-% drawGaussianSignal_Paper_v1(signal,time,mag,freq,typein)
+% plotGaussianSignal(signal,time,mag,freq,typein)
 %
-% Functions draws the time-domain (TD) signal generated with function
-% gaussianSignal,and the frequency spectrum of the signal defined by the
-% user as the function input. It also calculates back the frequency
-% spectrum of the output TD signal using Welch's estimator, and draws it
+% Functions draws a time-domain (TD) signal generated with function
+% gaussianSignal,and a frequency spectrum of the signal defined by the user
+% as the gaussianSignal input. It also calculates back the frequency
+% spectrum from the output TD signal using Welch's estimator, and draws it
 % for comparison.
 %
-% Inputs:
+% Created by Ales Skorjanc 18.2.2022
+%
+%% Inputs:
 % 
-% signal... TD signal returned by function gaussianSignal
+% signal: row vector
+%   TD signal values returned by the function gaussianSignal
 %
-% time... TD signal time points returned by function gaussianSignal
+% time: row vector
+%   TD signal time points returned by the function gaussianSignal
 %
-% mag... amplitudes or power spectral density (psd) of the signal frequency
-% spectrum defined by the user as gaussianSignal input and returned by the
-% function in a form interpolated to frequencis in freq
+% mag: row vector
+%   amplitude spectral density (asd) or power spectral density (psd) of the
+%   signal frequency spectrum defined by the user as gaussianSignal input
+%   and returned by the function gaussianSignal in a form interpolated to
+%   frequencis in freq
 %
-% freq... equally spaced frequencies of the user defined frequency spectrum
-% of the signal, returned by function gaussianSignal
+% freq: row vector
+%   equally spaced frequencies from 0 to half of the sampling frequency
+%   (Nyquist frequency) of TD signal, returned by function gaussianSignal
 %
-% type... type of the input frequency spectrum, 'amplitude' or 'power'
-
-
-%% scale the TD signal to the amplitude used as an input into the function gaussianSignal
-
-N = length(signal);
-% N... length 0f the TD signal
-df = freq(2)-freq(1);
-% df...frequency resolution in FD 
-fN = max(freq);
-% fN... Nyquist frequency
-
+% type: string
+%   type of the frequency spectrum used as an input into the function
+%   gaussianSignal, 'amplitude' or 'power'
 
 %% calculated psd of the TD signal using Welch's estimator
 
-fftWindow=2^(nextpow2(N)-4); 
-% fftWindow... fft window length 
-averagingWindow=2^(nextpow2(N)-7); 
-% averagingWindow... averaging window length
-noverlap=0; 
-% noverlap... window overlap 
+N = length(signal); % length of the TD signal
 
+df = freq(2)-freq(1); % frequency resolution in FD 
+
+fN = max(freq); % Nyquist frequency
+
+fftWindow=2^(nextpow2(N)-0); % fft window length 
+
+averagingWindow=2^(nextpow2(N)-7); % averaging window length
+
+noverlap=0; % averaging window overlap 
+
+% estimate psd of the signal using Welch's estimator, where sampling
+% frequency equals 2*fN
 [psd,psdf] = pwelch(signal,averagingWindow,noverlap,fftWindow,2*fN); 
-% estimate psd of the signal using Welch's estimator
 
-if strcmp(typein,'amplitude')
-    magin = sqrt(psd*sum(mag.^2)*df); 
-    % if the input into the function gaussianSignal was defined as
-    % amplitude spectrum, convert psd back to amplitude by taking the
-    % square root of psd and reversing the normalization of the signal
-    % variance to 1
-elseif strcmp(typein,'power')
-    magin = psd*(sum(mag)*df);
-    % if the input into the function gaussianSignal was defined as psd,
-    % reverse the normalization of the signal variance to 1
+%% recalculate frequency spectrum amplitude from psd and reverse normalization
+
+% if the input into the function gaussianSignal was defined as asd, convert
+% psd back to asd by taking the square root of psd and reversing the
+% normalization of the signal variance to 1. If the input into
+% gaussianSignal was defined as psd, only reverse the normalization of the
+% signal variance to 1.
+
+if strcmp(typeIn,'amplitude')
+    magIn = sqrt(psd*sum(mag.^2)*df);  
+elseif strcmp(typeIn,'power')
+    magIn = psd*(sum(mag)*df);
 end;
 
 
@@ -63,51 +69,53 @@ end;
 
 figure;
 
-% Plot the generated TD signal
+% Plot TD signal
 subplot(4,1,1);
 plot(time,signal);
 title('Normalized output signal in time domain')
 xlabel('time [s]')
 ylabel('amplitude')
 
-% Plot user defined signal amplitude in FD 
+% Plot user defined frequency spectrum, used as input for gaussianSignal 
 subplot(4,1,2);
 plot(freq,mag);
 
-if strcmp(typein,'amplitude')
+if strcmp(typeIn,'amplitude')
    title('User defined input amplitude spectrum')
-   ylabel('amplitude')
-elseif strcmp(typein,'power')
-    title('User defined input psd')
+   ylabel('asd')
+elseif strcmp(typeIn,'power')
+    title('User defined input power spectrum')
     ylabel('psd')
 end;
 
 xlabel('frequency [Hz]')
 
 
-% Plot the amplitude spectrum of the generated signal, estimated with
-% pwelch, to test the ouput of the algorithm
+% Plot the amplitude or power spectrum of the TD signal, estimated with
+% pwelch, to test the ouput of the gaussianSignal algorithm
 
 subplot(4,1,3);
-plot(psdf,magin); 
+plot(psdf,magIn); 
 
-if strcmp(typein,'amplitude')
+if strcmp(typeIn,'amplitude')
    title('Amplitude spectrum of the output TD signal estimated with pwelch')
-   ylabel('amplitude')
-elseif strcmp(typein,'power')
-    title('psd of the output TD signal estimated with pwelch')
+   ylabel('asd')
+elseif strcmp(typeIn,'power')
+    title('Power spectrum of the output TD signal estimated with pwelch')
     ylabel('psd')
 end;
 
 xlabel('frequency [Hz]')
 
 
-% Plot the distribution of signal's TD amplitudes to test for their
-% Gaussian distribution
+% Plot the distribution of values of TD signal to test for their Gaussian
+% distribution
+
 subplot(4,1,4);
 histogram(signal); 
 set(gca,'XLim',[-4,4]);
-title('Distribution of TD signal amplitudes')
+
+title('Distribution of TD signal values')
 xlabel('amplitude')
 ylabel('N')
 
